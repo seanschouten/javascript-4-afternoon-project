c5fdfd5f4d14601bb7e10a288e7dd1c8e0efef79
Sean Schouten <seanmschouten@gmail.com>
2019-05-17 15:33:22 -0600

    completed classes

--- classes.js
+++ classes.js
@@ -31,6 +31,18 @@
 
 //Code Here
 
+class Employee {
+  constructor(first_name, last_name, email, age){
+    this.first_name = first_name;
+    this.last_name = last_name;
+    this.email = email;
+    this.age = age;
+  }
+
+  makeWidget() {
+    return `${this.first_name} ${this.last_name} Widget`
+  }
+}
 
 ////////// PROBLEM 2 //////////
 
@@ -48,6 +60,19 @@
 */
 
 //Code Here
+
+class Manager extends Employee{
+  constructor(first_name, last_name, email, age){
+    super(first_name, last_name, email, age)
+    this.reports = []
+  }
+  hire(employee){
+   this.reports.push(employee)
+  }
+  fire(index){
+    this.reports.splice(index, 1)
+  }
+}
 
 
 ////////// PROBLEM 3 //////////
@@ -72,6 +97,39 @@
 */
 
 //Code Here
+
+class ProgressiveManager extends Manager {
+  constructor(first_name, last_name, email, age, reports, title='Not a manager', bonus= 0){
+    super(first_name, last_name, email, age, reports)
+    this.title = title
+    this.bonus = bonus
+  }
+  
+  hire(){
+    super.hire()
+    if (this.reports.length === 0){
+      return this.title = 'Not a Manager'
+    }else if
+      (this.reports.length >= 1 && this.reports.length <= 3){
+        return this.title = 'Barely Manager'
+    }else if 
+      (this.reports.length >= 4 && this.reports.length <= 10){
+        return this.title = 'Mostly Manager'
+    }else if 
+      (this.reports.length >= 11 && this.reports.length <= 50){
+      return this.title = 'Manager'
+    }else if 
+    (this.reports.length >= 51 && this.reports.length <= 100){
+    return this.title = 'Manager Plus'
+    }else {
+      return this.title = 'Bestest Manager' }
+    }
+    fire(){
+      super.fire()
+      this.bonus+= 100
+    }
+  }
+  
 
 
 
